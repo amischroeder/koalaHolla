c7c2fc475b9895b4a9bc33c15e7f720612dc7d18
Ami Schroeder <arschroeder22@gmail.com>
2017-08-10 16:55:11 -0500

    set-up project

--- server/server.js
+++ server/server.js
@@ -3,9 +3,11 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require( 'path' );
 var port = process.env.PORT || 5000;
+var koalas = require('./routes/koalas');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('server/public'));
+app.use(express.static('public'));
+app.use('/koalas', koalas);
 
 // Start listening for requests on a specific port
 app.listen(port, function(){
