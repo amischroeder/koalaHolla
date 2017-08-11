36fab857ae3529e409197a87c147c109a8c4f9cc
Ami Schroeder <arschroeder22@gmail.com>
2017-08-11 15:16:07 -0500

    added delete button

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,13 +1,13 @@
-console.log( 'js' );
+console.log('js');
 
-$( document ).ready( function(){
-  console.log( 'JQ' );
+$(document).ready(function () {
+  console.log('JQ');
   // load existing koalas on page load
   getKoalas();
 
   // add koala button click
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
+  $('#addButton').on('click', function () {
+    console.log('in addButton on click');
     // get user input and put in an object
     // NOT WORKING YET :(
     // using a test object
@@ -24,50 +24,65 @@ $( document ).ready( function(){
       notes: notesIn
     };
     // call saveKoala with the new obejct
-    saveKoala( objectToSend );
+    saveKoala(objectToSend);
   }); //end addButton on click
+  $('#viewKoalas').on('click', '.deleteButton', function () {
+    console.log('ready to kill koalas')
+    var koalaId = $(this).parent().data().id;
+    $.ajax({
+      method: 'DELETE',
+      url: '/koalas/' + koalaId,
+      success: function(response) {
+        getKoalas();
+      }
+
+    })
+
+  })
 }); // end doc ready
 
-function getKoalas(){
-  console.log( 'in getKoalas' );
+
+function getKoalas() {
+  console.log('in getKoalas');
   // ajax call to server to get koalas
   $.ajax({
     url: '/koalas',
     type: 'GET',
-    success: function( data ){
-      console.log( 'got some koalas: ', data );
+    success: function (data) {
+      console.log('got some koalas: ', data);
       displayKoalas(data);
     } // end success
   }); //end ajax
   // display on DOM with buttons that allow edit of each
 } // end getKoalas
 
-function saveKoala( newKoala ){
-  console.log( 'in saveKoala', newKoala );
+function saveKoala(newKoala) {
+  console.log('in saveKoala', newKoala);
   // ajax call to server to get koalas
   $.ajax({
     url: '/koalas',
     type: 'POST',
     data: newKoala,
-    success: function( data ){
-      console.log( 'got some koalas: ', data );
+    success: function (data) {
+      console.log('got some koalas: ', data);
       getKoalas(data);
     } // end success
   }); //end ajax
 }
 
-function displayKoalas(koalasArray){
+function displayKoalas(koalasArray) {
   $('#viewKoalas').empty();
 
-  for(var i=0; i <koalasArray.length; i++){
+  for (var i = 0; i < koalasArray.length; i++) {
     var koala = koalasArray[i];
-    var $koalaTR= $('<tr></tr>');
+    var $koalaTR = $('<tr></tr>');
     $koalaTR.data('id', koala.id);
     $koalaTR.append('<td class="name">' + koala.name + '</td>');
     $koalaTR.append('<td class="gender">' + koala.gender + '</td>');
     $koalaTR.append('<td class="age">' + koala.age + '</td>');
     $koalaTR.append('<td class="ready_for_transfer">' + koala.ready_for_transfer + '</td>');
     $koalaTR.append('<td class="notes">' + koala.notes + '</td>');
+    $koalaTR.append(' <button class="deleteButton">Delete Koala</button>')
     //$koalaTR.append('<td class="name">' + koala.name + '</td>');
     $('#viewKoalas').prepend($koalaTR);
   }
--- server/routes/koalas.js
+++ server/routes/koalas.js
@@ -45,4 +45,29 @@ router.post('/', function(req, res){
 	});
 });
 
+router.delete('/:id', function(req, res){
+	var koalaId = req.params.id; // messageId is 7
+	console.log('message delete was hit!');
+	pool.connect(function(errorConnectingToDatabase, client, done){
+		if(errorConnectingToDatabase) {
+			// when connecting to database failed
+			console.log('Error connecting to database', errorConnectingToDatabase);
+			res.sendStatus(500);
+		} else {
+			// when connecting to database worked!
+			// query like this: UPDATE messages SET message='Have a really terrific day!' WHERE id=1;
+			client.query('DELETE FROM koalas WHERE id=$1;', 
+							[koalaId], 
+							function(errorMakingQuery, result) {
+				done();
+				if(errorMakingQuery) {
+					console.log('Error making database query', errorMakingQuery);
+					res.sendStatus(500);
+				} else {
+					res.sendStatus(200);
+				}
+			});
+		}
+	});
+});
 module.exports = router;
